61873cca01d28fa1b5d8ccff13c5f84589595109
tapia1013 <manbearpig1013@gmail.com>
2021-04-22 12:20:05 -0700

    update our menu items to nav withRouterHOC

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -15,7 +15,7 @@ class Directory extends Component {
           title: 'hats',
           imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
           id: 1,
-          linkUrl: 'shop/hats'
+          linkUrl: 'hats'
         },
         {
           title: 'jackets',
@@ -57,9 +57,12 @@ class Directory extends Component {
     return (
       <div className="directory-menu">
         {
-          this.state.sections.map(({ title, imageUrl, id, size }) => {
+          this.state.sections.map(({ id, ...otherSectionProps }) => {
             return (
-              <MenuItem ket={id} title={title} imageUrl={imageUrl} size={size} />
+              <MenuItem
+                key={id}
+                {...otherSectionProps}
+              />
             )
           })
         }
