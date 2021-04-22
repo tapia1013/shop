b08174cf90509db534e0a02dae033f5a67961e27
tapia1013 <manbearpig1013@gmail.com>
2021-04-22 15:47:11 -0700

    up next is firebase

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import SignIn from '../../components/sign-in/sign-in.component';
+
+import './sign-in-and-sign-up.styles.scss';
+
+
+const SignInAndUpPage = () => {
+  return (
+    <div className="sign-in-and-sign-up">
+      <SignIn />
+    </div>
+  )
+}
+
+
+export default SignInAndUpPage;
