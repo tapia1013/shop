bca41b931aca6db7b5b057e342b515ac48c36b43
tapia1013 <manbearpig1013@gmail.com>
2021-04-27 14:49:16 -0700

    updated shop data with new data and updated cartutils to group items cart

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -4,10 +4,10 @@ import './custom-button.styles.scss'
 
 
 
-const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => {
+const CustomButton = ({ children, isGoogleSignIn, inverted, ...otherProps }) => {
   return (
     <button
-      className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`}
+      className={`${inverted ? 'inverted' : ''}  ${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`}
       {...otherProps}
     >
       {children}
