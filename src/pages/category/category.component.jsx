72cd8373a37a1e43fdc847e9a3594965858909c1
tapia1013 <manbearpig1013@gmail.com>
2021-05-01 14:21:20 -0700

    advanced routing correcting components and folder naming

---
+++ src/pages/category/category.component.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import CollectionItem from '../../components/collection-item/collection-item.component';
+
+
+import './category.styles.scss';
+
+
+
+
+const CategoryPage = ({ match }) => {
+  console.log(match);
+  return (
+    <div className="category">
+      <h2>Category Page</h2>
+    </div>
+  )
+}
+
+
+
+
+export default CategoryPage;
+
+
+
+
+
+
+/**
+ * ANNOYING NAMING OF COMPONENTS FROM VIDEO
+ * ADVANCED ROUTING VIDEO 2
+ *
+ *
+ */
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,16 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Route } from 'react-router-dom';
+
 
 
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component'
+import CategoryPage from '../category/category.component'
 
 
 
-
-const ShopPage = ({ collections }) => {
-
+const ShopPage = ({ match }) => {
+  console.log(match.params.categoryId);
   return (
     <div className="shop-page">
-      <CollectionsOverview />
+      <Route exact path={`${match.path}`} component={CollectionsOverview} />
+      <Route path={`${match.path}/:categoryId`} component={CategoryPage} />
     </div>
   )
 }
